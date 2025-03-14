f88d09886e1e9ca51b2396fa484461d211b7131c
HuangX <3221834658@qq.com>
2025-03-14 19:40:12 +0800

    nihao

--- src/main/java/com/hitwh/ShikeApplication.java
+++ src/main/java/com/hitwh/ShikeApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 public class ShikeApplication {
 
-    //测试测试
+  //你好吗
         public static void main(String[] args) {
             SpringApplication.run(ShikeApplication.class, args);
         }
